c00ec36b5f35500acb3cf74289400c73e0342df9
Andriy Lozynskiy <alozynskiy@gmail.com>
2023-11-27 02:50:56 +0200

    fix #9

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light">
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
-<body>
+<body class="theme-indigo">
 <div x-data="{
             showProfileDropdown: false,
             showMobileSidebar: false,
