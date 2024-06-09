e03f7d072106f16b8e1545f139a4bee7bee0eb90
Andriy Lozynskiy <alozynskiy@gmail.com>
2024-06-10 02:45:44 +0300

    update log viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Gate::define('viewLogViewer', function (?User $user) {
+            return true;
+        });
     }
 }
