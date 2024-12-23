beea36286a6fb068e48b25e0f38d9d93819b8887
Andriy Lozynskiy <alozynskiy@gmail.com>
2024-12-23 02:33:57 +0200

    fix pagination link

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -54,10 +54,10 @@
                     @elseif(is_array($element))
                         @foreach($element as $page => $url)
                             @if($page == $paginator->currentPage())
-                                <a href="javascript:void(0)"
+                                <span
                                    class="inline-flex items-center justify-center gap-2 rounded-lg border border-transparent bg-clr-50 px-4 py-2 font-semibold leading-6 text-clr-800 focus:ring focus:ring-clr-300/25 dark:bg-clr-700 dark:text-clr-100 dark:focus:ring-clr-600/40">
                                     {{$page}}
-                                </a>
+                                </span>
                             @else
                                 <a href="{{$url}}"
                                    class="inline-flex items-center justify-center gap-2 rounded-lg border border-transparent bg-white px-4 py-2 font-semibold leading-6 text-clr-700 hover:bg-clr-50 hover:text-clr-800 focus:ring focus:ring-clr-300/25 active:bg-transparent dark:bg-transparent dark:text-clr-300 dark:hover:bg-clr-700 dark:hover:text-clr-100 dark:focus:ring-clr-600/40 dark:active:bg-transparent">
