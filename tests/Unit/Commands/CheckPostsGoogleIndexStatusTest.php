bbd86268c882aa72b67bd2ae21526bdcecf2808b
Andriy Lozynskiy <alozynskiy@gmail.com>
2025-05-17 13:16:22 +0300

    trigger deploy

---
+++ tests/Unit/Commands/CheckPostsGoogleIndexStatusTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Commands;
+
+use App\Console\Commands\CheckPostsGoogleIndexStatus;
+use App\Models\Post;
+use App\Services\GoogleAPIService;
+use Tests\TestCase;
+use Tests\RefreshDatabaseCustom;
+
+class CheckPostsGoogleIndexStatusTest extends TestCase
+{
+    use RefreshDatabaseCustom;
+
+    public function test_checks_published_posts_index_status(): void
+    {
+        $publishedPost = Post::factory()->published()->create();
+        $post = Post::factory()->create();
+
+        $googleServiceMock = $this->mock(GoogleAPIService::class, function ($mock) use ($publishedPost) {
+            $mock->shouldReceive('getPageIndexStatus')->with('https://prostocode.com/posts/'. $publishedPost->slug)->once();
+        });
+        (new CheckPostsGoogleIndexStatus())->handle($googleServiceMock);
+    }
+}
